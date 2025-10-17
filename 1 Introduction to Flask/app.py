0a33cd41f1f8abff04777582bcbc8b46c35e11b3
ashish <ashishkr5443dsa@gmail.com>
2025-10-17 16:37:20 +0530

    make a another function for testing

--- 1 Introduction to Flask/app.py
+++ 1 Introduction to Flask/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
-app=Flask(__name__)
+app=Flask(__name__)  
 
-@app.route("/")   #create an endpoint
+@app.route("/")   # create an endpoint
+@app.route("/home")  # we can create multiple endpoints for the same function.
 def home():
     return "<h1>Welcome to the Home Page!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>Welcome to the about page!</h1>"
 
 if __name__=="__main__":
     app.run(debug=True)   # It will generate the log file.
