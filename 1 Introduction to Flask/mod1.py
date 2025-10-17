40e6b63aa1a9a19f95a140ff4347065727937c8c
ashish <ashishkr5443dsa@gmail.com>
2025-10-17 16:56:15 +0530

    update the file by taking the import module

--- 1 Introduction to Flask/mod1.py
+++ 1 Introduction to Flask/mod1.py
@@ -1,1 +1,3 @@
+# when we import any module, in that module all code will be executed and give the output of that module like mod2.
+import mod2
 print(f"Running mod1 - {__name__}")
--- 1 Introduction to Flask/mod2.py
+++ 1 Introduction to Flask/mod2.py
@@ -1,1 +1,4 @@
-print(f"Running mod2 - {__name__}")
+# print(f"Running mod2 - {__name__}")
+
+if __name__== "__main__":  # when i will execute the mod1.py then this condition will be false and this print statement will not be executed.
+    print(f"Running mod2 - {__name__}")
