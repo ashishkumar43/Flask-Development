8329a027d9ed8060efa293f107e49187bc5da54a
ashish <ashishkr5443dsa@gmail.com>
2025-10-26 22:23:11 +0530

    employees.py file added

---
+++ 3 Jinja, Templates, Inheritance/employees.py
@@ -0,0 +1,27 @@
+employees_data={
+    1:{
+        "name": "Ashish",
+        "age":21,
+        "position":"Employee"
+    },
+    2:{
+        "name": "Rohan",
+        "age":22,
+        "position":"manager"
+    },
+    3:{
+        "name": "Kishan",
+        "age":25,
+        "position":"TeamLead"
+    },
+    4:{
+        "name": "Dhruv",
+        "age":23,
+        "position":"Database Manager"
+    },
+    5:{
+        "name": "Gaurav",
+        "age":26,
+        "position":"DS Expert"
+    }
+}
