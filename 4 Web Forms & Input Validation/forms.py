81cf4a1704ab93fecf92e82ab9e83d0d89788da3
ashish <ashishkr5443dsa@gmail.com>
2025-10-31 21:55:59 +0530

    Added the forms.py file

---
+++ 4 Web Forms & Input Validation/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField,SelectField,DateField,PasswordField,SubmitField)
+from wtforms.validators import(DataRequired,Length,Email,optional,EqualTo)
+
+class SignupForm(FlaskForm):
+    username=StringField(
+        "Username",
+        validators=[DataRequired(),
+        Length(2,30)])
+    email=StringField(
+        "Email",
+        validators=[DataRequired()]
+    )
+    gender=SelectField("gender",choices=["Male","Female","Other"],validators=[optional()]
+    )
+    dob=DateField("Date of Birth",validators=[optional()]
+    )
+    password=PasswordField("Password",validators=[DataRequired(),Length(5,25)])
+    
+    Confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),Length(5,25),EqualTo("password")])
+     
+    submit=SubmitField("Signup")
+    
+
+
+class LoginForm(FlaskForm):
+    pass
