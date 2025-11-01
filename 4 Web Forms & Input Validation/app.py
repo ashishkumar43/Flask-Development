c4cf18ed2e0762e79fa73451b77734d248aaf396
ashish <ashishkr5443dsa@gmail.com>
2025-11-01 21:39:08 +0530

    app.py updated

--- 4 Web Forms & Input Validation/app.py
+++ 4 Web Forms & Input Validation/app.py
@@ -1,19 +1,27 @@
-from flask import Flask,render_template
+from flask import (Flask,render_template,redirect,url_for)
+
+from forms import SignupForm, LoginForm   # classes SignupForm and LoginForm 
 
 app=Flask(__name__)
+app.config["SECRET_KEY"]="THIS_IS_THE_SECRET_KEY"
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html" , title="Home")
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html" , title="Signup")
+@app.route("/signup", methods=["GET","POST"])
+def signup():  
+    form=SignupForm()   # create an object of that class
+    if form.validate_on_submit():
+        return redirect(url_for("home"))
+    return render_template("signup.html" , title="Signup",form=form)
+
 
 @app.route("/login")
 def login():
-    return render_template("login.html" , title="login")
+    form=LoginForm()    # create an object of that class
+    return render_template("login.html" , title="login",form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
