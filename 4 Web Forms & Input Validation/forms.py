9f40faa589cd04e8b6b3cfffa6ff617fd8e10e76
ashish <ashishkr5443dsa@gmail.com>
2025-11-03 21:44:50 +0530

    modified the forms.py

--- 4 Web Forms & Input Validation/forms.py
+++ 4 Web Forms & Input Validation/forms.py
@@ -4,33 +4,37 @@ from wtforms.validators import(DataRequired,Length,Email,optional,EqualTo)
 
 class SignupForm(FlaskForm):
     username=StringField(
-        "Username",
-        validators=[DataRequired(),
-        Length(2,30)])
+                         "Username",
+                         validators=[DataRequired(),Length(2,30)]
+                         )
     email=StringField(
-        "Email",
-        validators=[DataRequired()]
-    )
-    gender=SelectField("gender",choices=["Male","Female","Other"],validators=[optional()]
-    )
-    dob=DateField("Date of Birth",validators=[optional()]
-    )
-    password=PasswordField("Password",validators=[DataRequired(),Length(5,25)])
+                      "Email",
+                      validators=[DataRequired(),Email()]
+                      )
+    gender=SelectField(
+                       "Gender",choices=["Male","Female","Other"],validators=[optional()]
+                       )
+    dob=DateField("Date of Birth",validators=[optional()])
     
-    Confirm_password=PasswordField("Confirm Password",validators=[DataRequired(),Length(5,25),EqualTo("password")])
+    password=PasswordField(
+                           "Password",validators=[DataRequired(),Length(5,25)]
+                           )
+    
+    Confirm_password=PasswordField(
+                                    "Confirm Password",validators=[DataRequired(),Length(5,25),EqualTo("password")]
+                                    )
      
-    submit=SubmitField("Signup")
+    submit=SubmitField("Sign up")
     
-
-
 class LoginForm(FlaskForm):
     email=StringField(
         "Email",
-        validators=[DataRequired()]
+        validators=[DataRequired(),Email()]
     )
     
     password=PasswordField("Password",validators=[DataRequired(),Length(5,25)])
     
     remember_me= BooleanField("Remember Me")
+    
     submit=SubmitField("Login")
     
