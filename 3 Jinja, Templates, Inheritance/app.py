f68f96d648ec49dec130b50c393fb24c7da73c54
ashish <ashishkr5443dsa@gmail.com>
2025-11-06 21:09:06 +0530

    modified the app.py file

--- 3 Jinja, Templates, Inheritance/app.py
+++ 3 Jinja, Templates, Inheritance/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+from employees import employees_data
 
 # create the flask app 
 app=Flask(__name__)  # this have also one more parameter called templates it gives us the name templates that why i have made this folder name templates.
@@ -7,12 +8,33 @@ app=Flask(__name__)  # this have also one more parameter called templates it giv
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")  # invoke the template like home.html and all and then i will show on screen.
+    return render_template("home.html",title="HomePage")  # invoke the template like home.html and all and then i will show on screen.
 #about page 
 @app.route("/about")
-def about():
-    return render_template("about.html")
+def about():  
+    return render_template("about.html",title="aboutPage")
 
+#Contactus page 
+@app.route("/contactus")
+def about():  
+    return render_template("contact.html",title="contactusPage")
+
+
+# Newendpoint to find the number is even odd or zero..
+@app.route("/evaluate/<int:num>")
+def evaluate(num):
+    return render_template("evaluate.html",title="Evaluate",number=num)
+
+
+#Endpoint for employees data render.
+@app.route("/employees")
+def employees():
+    return render_template("employees.html",title="Employees",emps=employees_data)  
+
+#Endpoint for employees data render.
+@app.route("/employees/managers")
+def managers():
+    return render_template("managers.html",title="Managers",emps=employees_data)  
 
 #start the app
 if __name__=="__main__":
